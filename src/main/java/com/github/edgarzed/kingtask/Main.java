c789c52597c7bf308b48c1f06c7257b83bdc6cd3
edgger <germedgar@gmail.com>
2018-12-12 14:29:09 +0700

    Scanner nextInt() - slow fail

--- src/main/java/com/github/edgarzed/kingtask/Main.java
+++ src/main/java/com/github/edgarzed/kingtask/Main.java
@@ -1,55 +1,60 @@
 package com.github.edgarzed.kingtask;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        int size = Integer.parseInt(in.readLine());
-        String[] matrix = new String[size];
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int[][] matrix = new int[size][size];
         for (int i = 0; i < size; i++) {
-            matrix[i] = in.readLine();
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = in.nextInt();
+            }
         }
-        int requestNum = Integer.parseInt(in.readLine());
+        int requestNum = in.nextInt();
+        int[] requestParams = new int[5];
         for (int i = 0; i < requestNum; i++) {
-            String[] requestParams = in.readLine().split(" ");
-            if (requestParams.length == 5) {
+            int requestType = in.nextInt();
+            if (requestType == 1) {
+                for (int j = 0; j < 4; j++) {
+                    requestParams[j] = in.nextInt();
+                }
                 sum(matrix, requestParams);
             } else {
+                for (int j = 0; j < 3; j++) {
+                    requestParams[j] = in.nextInt();
+                }
                 modify(matrix, requestParams);
             }
         }
     }
 
-    private static void sum(String[] matrix, String[] requestParams) {
-        int x1 = Integer.parseInt(requestParams[2]);
-        int y1 = Integer.parseInt(requestParams[1]);
-        int x2 = Integer.parseInt(requestParams[4]);
-        int y2 = Integer.parseInt(requestParams[3]);
+    private static void sum(int[][] matrix, int[] requestParams) {
+        int x1 = requestParams[1];
+        int y1 = requestParams[0];
+        int x2 = requestParams[3];
+        int y2 = requestParams[2];
 
         long result = 0;
         for (; x1 <= x2; x1++) {
-            result += sumLine(matrix[x1].split(" "), y1, y2);
+            result += sumLine(matrix[x1], y1, y2);
         }
         System.out.println(result);
     }
 
-    private static long sumLine(String[] line, int y1, int y2) {
+    private static long sumLine(int[] line, int y1, int y2) {
         long result = 0;
         for (; y1 <= y2; y1++) {
-            result += Integer.parseInt(line[y1]);
+            result += line[y1];
         }
         return result;
     }
 
-    private static void modify(String[] matrix, String[] requestParams) {
-        int x = Integer.parseInt(requestParams[2]);
-        int y = Integer.parseInt(requestParams[1]);
-        String value = requestParams[3];
-        String[] newLine = matrix[x].split(" ");
-        newLine[y] = value;
-        matrix[x] = String.join(" ", newLine);
+    private static void modify(int[][] matrix, int[] requestParams) {
+        int x = requestParams[1];
+        int y = requestParams[0];
+        int value = requestParams[2];
+        matrix[x][y] = value;
     }
 }
