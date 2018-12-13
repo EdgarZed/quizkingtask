96f51031c03092aad8077ec23ffa72d6c0f47a6f
edgger <germedgar@gmail.com>
2018-12-13 09:03:07 +0700

    combine

--- src/main/java/com/github/edgarzed/kingtask/Main.java
+++ src/main/java/com/github/edgarzed/kingtask/Main.java
@@ -3,7 +3,11 @@ package com.github.edgarzed.kingtask;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static final int THRESHOLD = 100;
+
+    public static void main(String[] args) throws IOException {
+
         int size = readInt();
         int[][] matrix = new int[size][size];
         for (int i = 0; i < size; i++) {
@@ -11,29 +15,71 @@ public class Main {
                 matrix[i][j] = readInt();
             }
         }
+
         int requestNum = readInt();
-        int[] requestParams = new int[5];
+        if (requestNum < THRESHOLD) {
+            linearProc(matrix, requestNum);
+        } else {
+            satProc(matrix, requestNum, size);
+        }
+    }
+
+    private static void linearProc(int[][] matrix, int requestNum) throws IOException {
+        int requestType;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        int value;
+
         for (int i = 0; i < requestNum; i++) {
-            int requestType = readInt();
+            requestType = readInt();
             if (requestType == 1) {
-                for (int j = 0; j < 4; j++) {
-                    requestParams[j] = readInt();
-                }
-                sum(matrix, requestParams);
+                y1 = readInt();
+                x1 = readInt();
+                y2 = readInt();
+                x2 = readInt();
+                sum(matrix, x1, y1, x2, y2);
             } else {
-                for (int j = 0; j < 3; j++) {
-                    requestParams[j] = readInt();
-                }
-                modify(matrix, requestParams);
+                y1 = readInt();
+                x1 = readInt();
+                value = readInt();
+                matrix[x1][y1] = value;
             }
         }
     }
 
-    private static void sum(int[][] matrix, int[] requestParams) {
-        int x1 = requestParams[1];
-        int y1 = requestParams[0];
-        int x2 = requestParams[3];
-        int y2 = requestParams[2];
+    private static void satProc(int[][] matrix, int requestNum, int size) throws IOException {
+        long[][] computedMatrix = new long[size][size];
+        int requestType;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        int value;
+        int lastRequestType = 2;
+        for (int i = 0; i < requestNum; i++) {
+            requestType = readInt();
+            if (requestType == 1) {
+                if (lastRequestType == 2) {
+                    computeMatrix(size, matrix, computedMatrix);
+                }
+                y1 = readInt();
+                x1 = readInt();
+                y2 = readInt();
+                x2 = readInt();
+                System.out.println(calculateArea(computedMatrix, x1, y1, x2, y2));
+            } else if (requestType == 2) {
+                y1 = readInt();
+                x1 = readInt();
+                value = readInt();
+                matrix[x1][y1] = value;
+            }
+            lastRequestType = requestType;
+        }
+    }
+
+    private static void sum(int[][] matrix, int x1, int y1, int x2, int y2) {
         long result = 0;
         for (; x1 <= x2; x1++) {
             result += sumLine(matrix[x1], y1, y2);
@@ -47,13 +93,6 @@ public class Main {
             result += line[y1];
         }
         return result;
-    }
-
-    private static void modify(int[][] matrix, int[] requestParams) {
-        int x = requestParams[1];
-        int y = requestParams[0];
-        int value = requestParams[2];
-        matrix[x][y] = value;
     }
 
     private static int readInt() throws IOException {
@@ -73,4 +112,52 @@ public class Main {
         return negative ? result * -1 : result;
     }
 
+    private static void computeMatrix(int size, int[][] matrix, long[][] computedMatrix) {
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if (x > 0 && y > 0) {
+                    computedMatrix[x][y] = matrix[x][y] + computedMatrix[x - 1][y] + computedMatrix[x][y - 1] - computedMatrix[x - 1][y - 1];
+                } else if (x > 0) {
+                    computedMatrix[x][y] = matrix[x][y] + computedMatrix[x - 1][y];
+                } else if (y > 0) {
+                    computedMatrix[x][y] = matrix[x][y] + computedMatrix[x][y - 1];
+                } else {
+                    computedMatrix[x][y] = matrix[x][y];
+                }
+            }
+        }
+    }
+
+    // A O(1) time function to compute sum
+    // of submatrix between (x1, y1) and
+    // (x2, y2) using computedMatrix[][] which is
+    // built by the computeMatrix function
+    static long calculateArea(long[][] computedMatrix, int x1,
+                              int y1, int x2, int y2) {
+
+        // result is now sum of elements
+        // between (0, 0) and (x2, y2)
+        long res = computedMatrix[x2][y2];
+
+        // Remove elements between (0, 0)
+        // and (x1-1, y2)
+        if (x1 > 0) {
+            res = res - computedMatrix[x1 - 1][y2];
+        }
+
+        // Remove elements between (0, 0)
+        // and (x2, y1-1)
+        if (y1 > 0) {
+            res = res - computedMatrix[x2][y1 - 1];
+        }
+
+        // Add computedMatrix[x1-1][y1-1] as elements
+        // between (0, 0) and (x1-1, y1-1)
+        // are subtracted twice
+        if (x1 > 0 && y1 > 0) {
+            res = res + computedMatrix[x1 - 1][y1 - 1];
+        }
+
+        return res;
+    }
 }
