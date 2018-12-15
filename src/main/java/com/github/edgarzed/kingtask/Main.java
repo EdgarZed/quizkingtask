6856d47f14a3aa91d16ce468eb4f99746768e5f0
edgger <germedgar@gmail.com>
2018-12-15 22:36:57 +0700

    sat again...

--- src/main/java/com/github/edgarzed/kingtask/Main.java
+++ src/main/java/com/github/edgarzed/kingtask/Main.java
@@ -1,51 +1,82 @@
 package com.github.edgarzed.kingtask;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.io.IOException;
 
 public class Main {
-    private static final BlockingQueue<int[]> QUEUE = new LinkedBlockingQueue<>();
 
     public static void main(String[] args) throws Exception {
-
         int size = readInt();
         int[][] matrix = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[j][i] = readInt();
-            }
-        }
+        long[][] sat = new long[size][size];
+        fillMatrixAndSAT(matrix, sat);
 
         int requestsAmt = readInt();
         if (requestsAmt == 0) {
             return;
         }
-        Thread workerThread = new Thread(new LittleWorker(requestsAmt, matrix));
-        workerThread.start();
+        int[][] requests = new int[requestsAmt][5];
+        int lastSumRequest = getLastSumRequestAndFillRequests(requests);
 
-        for (int i = 0; i < requestsAmt; i++) {
-            int[] request = new int[5];
-            fillRequestData(request);
-            QUEUE.add(request);
+        for (int i = 0; i < lastSumRequest; i++) {
+            if (requests[i][0] == 1) {
+                System.out.println(calculateSum(sat, requests[i]));
+            } else {
+                int x = requests[i][1];
+                int y = requests[i][2];
+                matrix[x][y] = requests[i][3];
+                calcSat(matrix, sat, x, y);
+            }
         }
-
-        workerThread.join();
     }
 
-    private static void fillRequestData(int[] request) throws Exception {
-        request[0] = readInt();
-        if (request[0] == 1) {
-            for (int j = 1; j < 5; j++) {
-                request[j] = readInt();
+    private static void fillMatrixAndSAT(int[][] matrix, long[][] sat) throws IOException {
+        for (int y = 0; y < matrix.length; y++) {
+            for (int x = 0; x < matrix.length; x++) {
+                matrix[x][y] = readInt();
+                calcSatCell(matrix, sat, x, y);
             }
+        }
+    }
+
+    private static void calcSat(int[][] matrix, long[][] sat, int startX, int startY) {
+        for (int y = startY; y < matrix.length; y++) {
+            for (int x = startX; x < matrix.length; x++) {
+                calcSatCell(matrix, sat, x, y);
+            }
+        }
+    }
+
+    private static void calcSatCell(int[][] matrix, long[][] sat, int x, int y) {
+        if (x > 0 && y > 0) {
+            sat[x][y] = matrix[x][y] + sat[x - 1][y] + sat[x][y - 1] - sat[x - 1][y - 1];
+        } else if (x > 0) {
+            sat[x][y] = matrix[x][y] + sat[x - 1][y];
+        } else if (y > 0) {
+            sat[x][y] = matrix[x][y] + sat[x][y - 1];
         } else {
-            for (int j = 1; j < 4; j++) {
-                request[j] = readInt();
-            }
+            sat[x][y] = matrix[x][y];
         }
     }
 
-    private static int readInt() throws Exception {
+    private static int getLastSumRequestAndFillRequests(int[][] requests) throws IOException {
+        int lastSumRequest = 0;
+        for (int i = 0; i < requests.length; i++) {
+            requests[i][0] = readInt();
+            if (requests[i][0] == 1) {
+                for (int j = 1; j < 5; j++) {
+                    requests[i][j] = readInt();
+                }
+                lastSumRequest = i;
+            } else {
+                for (int j = 1; j < 4; j++) {
+                    requests[i][j] = readInt();
+                }
+            }
+        }
+        return lastSumRequest > 0 ? lastSumRequest + 1 : 1;
+    }
+
+    private static int readInt() throws IOException {
         int result = 0;
         boolean digital = false;
         boolean negative = false;
@@ -62,52 +93,24 @@ public class Main {
         return negative ? result * -1 : result;
     }
 
-    private static class LittleWorker implements Runnable {
+    private static long calculateSum(long[][] satMatrix, int[] request) {
+        int x1 = request[1];
+        int y1 = request[2];
+        int x2 = request[3];
+        int y2 = request[4];
 
-        private int requestsAmt;
-        private int[][] matrix;
+        long res = satMatrix[x2][y2];
 
-        private LittleWorker(int requestsAmt, int[][] matrix) {
-            this.requestsAmt = requestsAmt;
-            this.matrix = matrix;
+        if (x1 > 0) {
+            res = res - satMatrix[x1 - 1][y2];
+        }
+        if (y1 > 0) {
+            res = res - satMatrix[x2][y1 - 1];
+        }
+        if (x1 > 0 && y1 > 0) {
+            res = res + satMatrix[x1 - 1][y1 - 1];
         }
 
-        @Override
-        public void run() {
-            for (int i = 0; i < requestsAmt; i++) {
-                try {
-                    int[] requestData = QUEUE.take();
-                    if (requestData[0] == 1) {
-                        System.out.println(sum(requestData));
-                    } else if (requestData[0] == 2) {
-                        matrix[requestData[1]][requestData[2]] = requestData[3];
-                    }
-                } catch (InterruptedException e) {
-                    break;
-                }
-            }
-        }
-
-        private long sum(int[] requestData) {
-            int x1 = requestData[1];
-            int y1 = requestData[2];
-            int x2 = requestData[3];
-            int y2 = requestData[4];
-
-            long result = 0;
-            for (; x1 <= x2; x1++) {
-                result += sumLine(matrix[x1], y1, y2);
-            }
-            return result;
-        }
-
-        private long sumLine(int[] line, int y1, int y2) {
-            long result = 0;
-            for (; y1 <= y2; y1++) {
-                result += line[y1];
-            }
-            return result;
-        }
-
+        return res;
     }
 }
